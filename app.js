46b9448da421841b5349a31fab5dbe096aaed8aa
Pradnya Ghuge <gpradnya99@gmail.com>
2024-02-06 21:10:02 +0530

    parcel modification

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
 const heading1 = React.createElement(
   "h1",
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,36 @@
+const heading1 = React.createElement("h1", {
+    id: "title",
+    className: "class"
+}, "heading 1");
+const heading2 = React.createElement("h2", {
+    id: "title",
+    className: "class"
+}, "heading 2");
+const container = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading2
+]);
+//console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//passing react element inside the root
+root.render(container);
+const obj = {
+    firstname: "pradnya",
+    PrintName: ()=>{
+        console.log(this.firstname);
+    },
+    PrintName2: function() {
+        console.log(this.firstname);
+    }
+};
+const obj2 = {
+    firstname: "lee"
+};
+obj.PrintName();
+obj.PrintName2();
+obj.PrintName.call();
+obj.PrintName2.call();
+
+//# sourceMappingURL=index.7c0ccee6.js.map
